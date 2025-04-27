419a724f61bf7885d9841797cb76ae7455dd6f50
Pankaj Kumar <pankajkumar.software@gmail.com>
2025-04-27 12:23:37 +0530

    Added 2D array

---
+++ step0_Beginner_To_Adv_java/CountOfUnnqueelements.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class CountOfUnnqueelements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
---
+++ step0_Beginner_To_Adv_java/SelfTry.java
@@ -0,0 +1,6 @@
+public class SelfTry {
+
+    public static void main(String[] args) {
+        System.out.print("SelfTry by Pks");
+    }
+}
